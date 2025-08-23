49ff43cd54361631f25f5f8e1f61e680ceda9aca
andrey <a.starukhin@modernsys.ru>
2025-08-23 19:44:38 +0700

    Split calculatior to declaration and definition

--- CalculateTwoNumbers/Calculator.cpp
+++ CalculateTwoNumbers/Calculator.cpp
@@ -1,5 +1,53 @@
-//
-// Created by andrey on 8/18/25.
-//
-
 #include "Calculator.h"
+
+double Calculator::add() const {
+    return num1 + num2;
+}
+
+double Calculator::multiply() const {
+    return num1 * num2;
+}
+
+double Calculator::subtract_1_2() const {
+    return num1 - num2;
+}
+
+double Calculator::subtract_2_1() const {
+    return num2 - num1;
+}
+
+double Calculator::divide_1_2() const {
+    if (num2 == 0) {
+        std::cerr << "Ошибка: деление на ноль!" << std::endl;
+        return 0;
+    }
+    return num1 / num2;
+}
+
+double Calculator::divide_2_1() const {
+    if (num1 == 0) {
+        std::cerr << "Ошибка: деление на ноль!" << std::endl;
+        return 0;
+    }
+    return num2 / num1;
+}
+
+bool Calculator::set_num1(double num1) {
+    if (num1 != 0) {
+        this->num1 = num1;
+        return true;
+    }
+    return false;
+}
+
+bool Calculator::set_num2(double num2) {
+    if (num2 != 0) {
+        this->num2 = num2;
+        return true;
+    }
+    return false;
+}
+
+Calculator::Calculator() : num1(0), num2(0) {}
+
+Calculator::Calculator(double n1, double n2) : num1(n1), num2(n2) {}
--- CalculateTwoNumbers/Calculator.h
+++ CalculateTwoNumbers/Calculator.h
@@ -8,56 +8,24 @@ private:
     double num2;
 
 public:
-    Calculator() : num1(0), num2(0) {}
-    Calculator(double n1, double n2) : num1(n1), num2(n2) {}
+    Calculator();
+    Calculator(double n1, double n2);
 
-    double add() const {
-        return num1 + num2;
-    }
+    double add() const;
 
-    double multiply() const {
-        return num1 * num2;
-    }
+    double multiply() const;
 
-    double subtract_1_2() const {
-        return num1 - num2;
-    }
+    double subtract_1_2() const;
 
-    double subtract_2_1() const {
-        return num2 - num1;
-    }
+    double subtract_2_1() const;
 
-    double divide_1_2() const {
-        if (num2 == 0) {
-            std::cerr << "Ошибка: деление на ноль!" << std::endl;
-            return 0;
-        }
-        return num1 / num2;
-    }
+    double divide_1_2() const;
 
-    double divide_2_1() const {
-        if (num1 == 0) {
-            std::cerr << "Ошибка: деление на ноль!" << std::endl;
-            return 0;
-        }
-        return num2 / num1;
-    }
+    double divide_2_1() const;
 
-    bool set_num1(double num1) {
-        if (num1 != 0) {
-            this->num1 = num1;
-            return true;
-        }
-        return false;
-    }
+    bool set_num1(double num1);
 
-    bool set_num2(double num2) {
-        if (num2 != 0) {
-            this->num2 = num2;
-            return true;
-        }
-        return false;
-    }
+    bool set_num2(double num2);
 };
 
 #endif //CPPM_HOMEWORKS_CALCULATOR_H
